49c3efc8ec7758c1e991a44c2efadbf0ae3cd8dc
Max Zhang <maxzhang10124@gmail.com>
2024-05-22 22:47:59 -0700

    test

--- fridgeproject-1/src/main/java/edu/sdccd/cisc190/fridgeproject/UnitConverter.java
+++
@@ -1,6 +0,0 @@
-package edu.sdccd.cisc190.fridgeproject;
-
-public interface UnitConverter {
-double convert(double d);
-    
-}
---
+++ src/main/java/edu/sdccd/cisc190/fridgeproject/UnitConverter.java
@@ -0,0 +1,6 @@
+package edu.sdccd.cisc190.fridgeproject;
+
+public interface UnitConverter {
+double convert(double d);
+    
+}
